4443f23feb9a2fa5d56dbd8f6e76f307104fc62e
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-24 22:31:26 +0200

    24.09.2022

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// 
 
-const listEmage = document.querySelector('.gallery');
-const markup = galleryItems.map((galleryItem) => `<li class="items"><img class = "items-image" src = ${galleryItem.preview} alt = ${galleryItem.description} width="800" height="600"/></li>`)
-.join("");
+const galleryTemplate =({preview, original, description}) =>`
+<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`
+const imagesContainer = document.querySelector(".gallery");
+const galleryList = galleryItems.map(galleryItem => galleryTemplate(galleryItem)).join('');
+imagesContainer.insertAdjacentHTML("afterbegin", galleryList);
 
-listEmage.insertAdjacentHTML("beforeend", markup);
+//-------------------Modal window need to fix--------------------------
+
+imagesContainer.addEventListener('click', onImageHandleClick);
+const instance = {
+    openImageInModal (image) {
+        const instance = basicLightbox.create(`
+            <div class="modal">
+                <img src="${image.dataset.source}"/>
+            </div>`);
+        instance.show();
+    },
+    closeModal() {
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") {
+                console.log(e);
+                instance.close();
+            }
+        })
+    }
+}
+function onImageHandleClick (evt){
+    evt.preventDefault();
+    const isImage = evt.target.classList.contains('gallery__image');
+    if ( !isImage ) {return};
+    const activeImage = evt.target;
+   instance.openImageInModal(activeImage);
+    instance.closeModal();
+} 
+
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show();
