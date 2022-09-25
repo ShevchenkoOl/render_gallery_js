de91839dfca1dd0003e8f4282be8d63778239204
Oleksii Shevchenko <uzlabini@gmail.com>
2022-09-25 21:26:33 +0200

    25.09.2022

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryTemplate =({preview, original, description}) =>`
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+
+const imagesContainer = document.querySelector(".gallery");
+const galleryList = galleryItems.map(galleryItem => galleryTemplate(galleryItem)).join('');
+imagesContainer.insertAdjacentHTML("afterbegin", galleryList);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+	captionDelay: 250
+})
+
